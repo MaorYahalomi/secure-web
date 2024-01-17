9fbaa1590c80313cc7c38cc2b5efb4cd04f1f20c
MaorYahalomi <yahlomi@gmail.com>
2024-01-17 20:25:58 +0200

    update-vars

--- vars.tf
+++ vars.tf
@@ -1,19 +1,3 @@
-variable "AWS_ACCESS_KEY" {
-  default = "xx"
-}
-
-variable "AWS_SECRET_KEY" {
-  default = "xx"
-}
-
 variable "instance_type" {
   default = "t2.micro"
-}
-
-variable "AWS_REGION" {
-  default = "us-east-2"
-}
-
-variable "Slack_Webhook_URL" {
-  default = "https://username:password@slack.com/slack_webhook"
 }
